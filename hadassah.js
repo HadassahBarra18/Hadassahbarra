ece9fef717a7ad6d1fa62bc2651378e44f6c9179
HadassahBarra18 <hbarradelacruz@gmail.com>
2025-04-07 16:41:55 -0600

    Add files via upload

---
+++ hadassah.js
@@ -0,0 +1,13 @@
+function Saludar()
+{
+    let nombre=("Ingrese su nombre");
+    alert ("Hola" + nombre + "suerte en su examen");
+}
+
+
+function Calcularcuadrado()
+{
+    let numero= parseInt(prompt ("Ingrese el numero"));
+    let numcuadrado= num^
+    alert ("El cuadrado es" + numcuadrado);
+}
